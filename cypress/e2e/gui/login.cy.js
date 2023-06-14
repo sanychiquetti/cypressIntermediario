8855df6a5eca594ec70e7f8b3ce8223a2abf93c0
sanychiquetti <sanymara77@hotmail.com>
2023-06-14 20:07:07 -0300

    refatorando o código

--- cypress/e2e/gui/login.cy.js
+++ cypress/e2e/gui/login.cy.js
@@ -2,6 +2,5 @@ describe('Login', () => {
     it('successfully', () => {
       cy.login()
       cy.get('.qa-user-avatar').should('be.visible')
-      cy.get('[data-qa-selector="welcome_title_content"]').contains("Welcome to GitLab")
     })
   })
