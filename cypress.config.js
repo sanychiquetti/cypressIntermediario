21484843c4d785fbd178011ee778365866155602
sanychiquetti <sanymara77@hotmail.com>
2023-06-14 19:19:28 -0300

    criando pastas

---
+++ cypress.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost',
+  },
+  fixturesFolder: false, //não vamos usar a pasta fisture, então desligamos essa pasta para o cypress não cria-la novamente
+  video: false, // estamos desligando a geração de videos
+})
