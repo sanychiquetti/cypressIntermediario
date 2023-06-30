5f6734d4895a16d4f9c150371a3bb7f96bce91a5
sanychiquetti <sanymara77@hotmail.com>
2023-06-30 20:15:00 -0300

    Criando testes de API e comando de limpar projetos criados

--- cypress/e2e/gui/createProject.cy.js
+++ cypress/e2e/gui/createProject.cy.js
@@ -3,20 +3,18 @@ import { faker } from '@faker-js/faker'
 describe('Create Project', () => {
     //pré-condição:
     beforeEach(() => {
+        cy.api_deleteProjects()
         cy.login()
         cy.visit('/')
     })
-
     it('successfully', () => {
         //vamos colocar em uma variável pois vamos usar outras vezes no projeto, então é só chamar o objeto: project
         const project = {
             name: `project-${faker.datatype.uuid()}`, //chamando a forma randomica pelo faker
             description: faker.random.words(5) //usando a forma randomica com o
         }
-
         //ação:
         cy.gui_createProject(project) //comando criado na pasta gui_commands
-
         //resultados esperados/validações:
         cy.url().should('be.equal', `${Cypress.config('baseUrl')}/${Cypress.env('user_name')}/${project.name}`)
         cy.contains(project.name).should('be.visible')
